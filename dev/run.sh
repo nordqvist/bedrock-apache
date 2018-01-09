8ab9e1907b81d35a103405ad069d073d48c8d521
Björn Nordqvist <bjorn@nordqvist.io>
2018-01-09 11:16:39 +0100

    Bugfixes and install changes

--- dev/run.sh
+++ dev/run.sh
@@ -4,3 +4,4 @@ echo "Cleaning up!"
 apt-get clean
 rm -r /var/lib/apt/lists/*
 echo "Starting Apache!"
+apache2-foreground
--- production/run.sh
+++ production/run.sh
@@ -6,3 +6,4 @@ rm -r /var/lib/apt/lists/*
 rm /usr/local/bin/wp
 rm /usr/local/bin/composer
 echo "Starting Apache!"
+apache2-foreground
